dff856c7ae11aec01428e8c8818693cc6dd863ec
Aaron Franks <aaron.franks@gmail.com>
2014-12-16 21:29:02 -0800

    Split out css/js to a separate file.

---
+++ deck.js
@@ -0,0 +1,26 @@
+var mystack = stack()
+
+var $ = function(query) {
+    return [].slice.call(document.querySelectorAll(query));
+};
+
+// Get all code blocks in the presentation and either eval their code,
+// or make the code runnable when the code is clicked:
+$('pre > code').forEach(function(el) {
+    (function(codeTag) {
+        if (codeTag.classList.contains('norun')) {
+            // Do nothing
+        } else if (codeTag.classList.contains('predefine')) {
+            var code = codeTag.innerText;
+            eval(code);
+        } else {
+            codeTag.classList.add('clickable');
+            codeTag.addEventListener('dblclick', function(evt) {
+                var code = codeTag.innerText;
+                eval(code);
+                codeTag.classList.add('finished');
+                setTimeout(function() { codeTag.classList.remove('finished'); }, 7000);
+            })
+        }
+    }(el))
+});
